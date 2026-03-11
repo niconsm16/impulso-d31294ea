a8129aad9677cce45b323eeb729066fd168020c9
nico <nicolas.machicado@bue.edu.ar>
2026-03-11 10:56:10 -0300

    vite.config.ts - base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: "/impulso-d31294ea/",
+  base: "/impulso-d31294ea/",
   server: {
     host: "::",
     port: 8080,
