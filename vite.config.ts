621d2fbd708f388628e89ff2f0891674aa9e3c77
nico <nicolas.machicado@bue.edu.ar>
2026-03-11 11:24:10 -0300

    test: vite css plugin

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,23 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    {
+      name: "fix-css-urls",
+      generateBundle(_, bundle) {
+        for (const file of Object.values(bundle)) {
+          if (file.type === "asset" && file.fileName.endsWith(".css")) {
+            file.source = (file.source as string).replace(
+              /url\(['"]?assets\//g,
+              `url('/tu-repo/assets/`,
+            );
+          }
+        }
+      },
+    },
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
