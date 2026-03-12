5fb4740cd863e5242c30d099ff6a695c1f13e790
nico <nicolas.machicado@bue.edu.ar>
2026-03-12 10:56:15 -0300

    fix: repository init path

--- src/components/global/icons.tsx
+++ src/components/global/icons.tsx
@@ -2,12 +2,14 @@ import SystemIcon from "../icons/SystemIcon";
 import { globalIcons } from "./data";
 import "./style.css";
 
+const repositorio = "impulso-d31294ea/";
+
 export default function Icons() {
   return (
     <div className={"global-icons"}>
       {globalIcons.map((icon) => (
         <div className={"icon"} key={icon.link}>
-          <a href={icon.link} target="_self" title={icon.title}>
+          <a href={repositorio + icon.link} target="_self" title={icon.title}>
             <icon.component />
           </a>
         </div>
